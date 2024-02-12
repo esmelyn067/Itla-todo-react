6babaa0e8daef98855750fe0d75fe097d6709188
Esmelyn M. Santos <esmelyn@mail.com>
2024-02-12 00:16:38 -0400

    iniciando tarea react-todo-list

---
+++ todo-react-itla/src/app.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'preact/hooks'
+import './app.css'
+
+export function App() {
+
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    // agrega la tarea a la lista
+    setList([...list, newTodo]);
+
+    // deja campo vacio 
+    setInput("");
+  };
+
+  const deleteTodo = (id) => {
+    // Filtrar tarea con el ID
+    const newList = list.filter((todo) => todo.id !== id);
+
+    setList(newList);
+  };
+
+  return (
+    <>
+     <h1>Todo List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
